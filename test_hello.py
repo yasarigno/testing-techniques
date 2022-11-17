a5e9c574902edb1867e91b74e43635e0e227aea4
firat <yasar.f@sfeir.com>
2022-11-17 16:42:43 +0100

    try 4

--- test_hello.py
+++ test_hello.py
@@ -1,6 +1,7 @@
 import pytest
 from hello import addThis
 
-assert addThis(4, 8) == 12
-assert addThis("1",2) == 3
-assert addThis(3,5) == 8        # gives a false error if you set it != 8
+def test_addThis():
+    assert addThis(4, 8) == 12
+    assert addThis("1",2) == 3
+    assert addThis(3,5) == 8        # gives a false error if you set it != 8

3a5ab5e8aecce25ce14d8d6ea1144ac5d82f515b
firat <yasar.f@sfeir.com>
2022-11-17 16:16:11 +0100

    a bug is fixed on the test_hello file modified

--- test_hello.py
+++ test_hello.py
@@ -2,4 +2,4 @@ from hello import addThis
 
 assert addThis(4, 8) == 12
 assert addThis("1",2) == 3
-assert addThis(3,5) == 9        # gives a false error
+assert addThis(3,5) == 8        # gives a false error if you set it != 8
